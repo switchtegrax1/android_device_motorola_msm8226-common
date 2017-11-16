eaafd6878e056badcfb9e13694501fa73bbcf2a6
Bruno Martins <bgcngm@gmail.com>
2017-11-16 22:40:39 +0000

    msm8226-common: Include DRM service
    Change-Id: I4999809c288c84d6e9c39383ff76f9ee85a97cae
    Signed-off-by: RenanQueiroz <queirozrrq@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -111,7 +111,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
