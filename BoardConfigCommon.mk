73722618b7c0f6e4238acf089b4caf0f1c49c42d
Andersonmendes2016 <lge500meu@gmail.com>
2017-12-19 18:07:02 -0200

    msm8226-common: libshim: Add missing symbol for camera
    01-06 13:20:40.146   501   501 F DEBUG   : pid: 392, tid: 392, name: mm-qcamera-daem  >>> /system/bin/mm-qcamera-daemon <<<
    01-06 13:20:40.146   501   501 F DEBUG   : signal 6 (SIGABRT), code -6 (SI_TKILL), fault addr --------
    01-06 13:20:40.150   501   501 F DEBUG   : Abort message: 'CANNOT LINK EXECUTABLE "/system/bin/mm-qcamera-daemon": cannot locate symbol "_ZN8SkCanvas14drawRectCoordsEffffRK7SkPaint" referenced by "/system/vendor/lib/libqc-opt.so"...'
    
    Change-Id: Ie3fb400bf93fde0843e96e58c15d1e3168d4a865

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,7 +160,9 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/hw/camera.vendor.msm8226.so|libshims_camera.so \
     /system/lib/libmot_sensorlistener.so|libshims_sensorlistener.so \
     /system/vendor/lib/libqc-opt.so|libshim_qcopt.so \
-    /system/lib/libmdmcutback.so|libqsap_shim.so 
+    /system/vendor/lib/libqc-opt.so|libshims_sensorlistener.so \
+    /system/lib/libmdmcutback.so|libqsap_shim.so
+
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
