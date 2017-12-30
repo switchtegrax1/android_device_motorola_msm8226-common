8e849ef93d2184d1e9eea1630842857661bef3cb
Paul Keith <javelinanddart@gmail.com>
2017-12-30 23:56:33 +0100

    msm8226-common: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c
    Signed-off-by: RenanQueiroz <queirozrrq@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -178,8 +178,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8226
+    android.hardware.power@1.0-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
